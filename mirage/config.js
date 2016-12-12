518a1b9d224eb04c1ce8530b988c2489445312cc
PhelanLewis <phelanlewis@gmail.com>
2016-12-11 22:04:50 -0800

    setup mirage and started filter for listing

--- mirage/config.js
+++ mirage/config.js
@@ -1,29 +1,32 @@
 export default function() {
        this.namespace = '/api';
 
-        this.get('/rentals', function() {
+        this.get('/vegetables', function() {
           return {
             data: [{
-              id: 'vegetable',
-              title: 'Broccoli',
-              attributes:{
+              id: 'Broccoli',
+              type: 'Vegetable',
+              attributes: {
+                title: 'Broccoli',
                 sun: 'Full Sun',
                 plantingseason: 'April to July',
                 image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Broccoli_and_cross_section_edit.jpg/800px-Broccoli_and_cross_section_edit.jpg'
               }
                 }, {
-              id: 'vegetable',
-              title: 'Tomato',
+              id: 'Tomato',
+              type: 'Vegetable',
               attributes:  {
+                title: 'Tomato',
                 sun: 'Full Sun',
                 plantingseason: 'March to July',
                 image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Tomato_fruit_and_flowers_at_day_52.jpg/800px-Tomato_fruit_and_flowers_at_day_52.jpg'
               }
                 }, {
-              id: 'vegetable',
-              title: 'Potato',
+              id: 'Potato',
+              type: 'Vegetable',
               attributes: {
-                 sun: 'Partial and Full Sun',
+                title: 'Potato',
+                sun: 'Partial and Full Sun',
                 plantingseason: 'March to July',
                 image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/800px-Patates.jpg'
               }
